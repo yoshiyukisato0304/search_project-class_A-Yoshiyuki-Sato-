52d8938435313271d82760b41740d26fe08c2f1a
yoshiyukisato0304 <yoshiyuki.sato0304@gmail.com>
2024-11-09 23:16:11 +0900

    画像データを扱えるようデータベースを修正

--- search_project/search_app/models.py
+++ search_project/search_app/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='images/', null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=0)
     # 1 はカテゴリ ID
     def __str__(self):
