f59d5a46575a45ea8a9b03528c9d23adc72e40c1
yoshiyukisato0304 <yoshiyuki.sato0304@gmail.com>
2024-12-07 10:47:32 +0900

    env追加

--- search_project/search_project/settings.py
+++ search_project/search_project/settings.py
@@ -15,19 +15,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 import os
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n2p%!td7k=r80bd3cabww$r($(ns8fbqpn!00xwq%zy*8q)xsm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+
 
 ALLOWED_HOSTS = []
 
@@ -81,15 +86,19 @@ WSGI_APPLICATION = 'search_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#  'default': {
+#  'ENGINE': 'django.db.backends.postgresql',
+#  'NAME': 'search_db',
+#  'USER': 'postgres',
+#  'PASSWORD': 'postgres',
+#  'HOST': 'localhost',
+#  'PORT': '5432',
+#  }
+# }
+
 DATABASES = {
- 'default': {
- 'ENGINE': 'django.db.backends.postgresql',
- 'NAME': 'search_db',
- 'USER': 'postgres',
- 'PASSWORD': 'postgres',
- 'HOST': 'localhost',
- 'PORT': '5432',
- }
+    'default': env.db(),
 }
 
 
