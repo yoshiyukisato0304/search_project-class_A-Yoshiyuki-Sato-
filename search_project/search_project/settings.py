0225afe1a1f8dc311ce7babc17b7035d3e95980b
yoshiyukisato0304 <yoshiyuki.sato0304@gmail.com>
2024-12-20 14:41:24 +0900

    debugがfalseにならないのを修正

--- search_project/search_project/settings.py
+++ search_project/search_project/settings.py
@@ -31,10 +31,11 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+#ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
